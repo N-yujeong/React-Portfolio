4b541871b64dc81632323325ed645caeea8e533f
남유정 <your_email@example.com>
2024-12-16 04:53:39 +0900

    Add new page and layout files

---
+++ app/Data/intros.js
@@ -0,0 +1,13 @@
+import Resume from "../introduction/Resume/page";
+import SelfIntro from "../introduction/SelfIntro/page";
+
+const intros = [
+  {
+    tab: "이력서",                            // 메뉴 버튼
+    content: <Resume></Resume>,   // 컴포넌트 
+  },
+  {
+    tab: "자기소개서",
+    content: <SelfIntro></SelfIntro>,
+  },
+];
