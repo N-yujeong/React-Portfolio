b30c332f9f70f02bbc4c8dca7ce299b9ca2be935
남유정 <your_email@example.com>
2024-12-16 20:54:07 +0900

    Add new page and layout files

--- app/career/career2/page.js
+++ app/career/career2/page.js
@@ -1,17 +1,18 @@
-import styles from "./Career.module.css";
+import styles from './Career.module.css';
 
-function Career2() {
-  return (
-    <div className={styles.container}>
-      <fieldset className={styles.fieldset}>
-        <legend className={styles.legend}>네트워크 관리사 2급</legend>
-        <ul className={styles.list}>
-          <li>자격의 종류: 공인민간자격</li>
-          <li>자격발급기관: 한국정보통신자격협회</li>
-        </ul>
-      </fieldset>
-    </div>
-  );
-}
+const Career2 = () => {
+    return (
+        <div className={styles.container}>
+            <fieldset>
+                <legend>네트워크 관리사 2급 (필기)</legend>
+                <ul>
+                    <li>자격의 종류: 공인민간자격</li>
+                    <li>자격발급기관: 한국정보통신자격협회</li>
+                    <li>기타: 2024.12.01 실시시험 응시 예정</li>
+                </ul>
+            </fieldset>
+        </div>
+    );
+};
 
 export default Career2;
