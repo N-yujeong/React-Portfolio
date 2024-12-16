216c55c83d23317f0ce7005c3e4195aac3844219
남유정 <your_email@example.com>
2024-12-16 20:56:53 +0900

    Add new page and layout files

--- app/introduction/page.js
+++ app/introduction/page.js
@@ -1,19 +1,31 @@
-'use client';
-import { useState } from 'react';
-import intros from '../Data/intros';
+'use client'     // 클라이언트 컴포넌트
 
-export default function Introduction() {
-  const [selectedIndex, setSelectedIndex] = useState(0);
+import { useState } from "react";
+import intros from "../Data/intros";  // 데이터 파일 
+
+function Introduction() {
+  const [index, setIndex] = useState(0);
+
+  const contentChange = (event) => {
+    setIndex(event.target.value);
+  };
 
   return (
     <div>
-      {intros.map((intro, index) => (
-        <button key={index} onClick={() => setSelectedIndex(index)}>
+      {intros.map((intro, idx) => (
+        <button key={idx} onClick={contentChange} value={idx}>
           {intro.tab}
         </button>
       ))}
-      <br /><br />
-      {intros[selectedIndex].content}
+      
+      <br /> <br />
+
+      <div>
+        {intros[index]?.content}
+      </div>
+
     </div>
   );
 }
+
+export default Introduction;
